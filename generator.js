83e2c8e218e188ba563ff45e6d0d2d38e6684e4b
sunita-meena55 <94470143+sunita-meena55@users.noreply.github.com>
2022-07-27 11:06:07 +0530

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,18 @@
+var qr;
+(function() {
+        qr = new QRious({
+        element: document.getElementById('qr-code'),
+        size: 200,
+        value: 'https://studytonight.com'
+    });
+})();
+
+function generateQRCode() {
+    var qrtext = document.getElementById("qr-text").value;
+    document.getElementById("qr-result").innerHTML = "QR code for " + qrtext +":";
+    qr.set({
+        foreground: 'black',
+        size: 200,
+        value: qrtext
+    });
+}
